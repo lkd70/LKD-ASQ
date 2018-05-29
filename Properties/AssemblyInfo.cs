27c55c2bff025d4225729b20c4cedba745a8f498
LKD70 <lkdornan70@gmail.com>
2018-05-29 19:13:40 +0100

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LKD ASQ")]
+[assembly: AssemblyDescription("LKD70's Ark Server Query")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LKDVPS")]
+[assembly: AssemblyProduct("LKD ASQ")]
+[assembly: AssemblyCopyright("Copyright © 2018 LKD70")]
+[assembly: AssemblyTrademark("LKD70")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a4d120a-e4a4-470a-bc5e-87254a2e1c23")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
